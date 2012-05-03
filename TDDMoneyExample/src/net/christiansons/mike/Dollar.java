20fd070a981fd251c96afa080fe1871e9c1373f7
Mike Christianson <mike.christianson@gmail.com>
2012-05-02 20:40:57 -0700

    Chapter 11: removed unnecessary subclasses

--- TDDMoneyExample/src/net/christiansons/mike/Dollar.java
+++
@@ -1,9 +0,0 @@
-package net.christiansons.mike;
-
-public class Dollar extends Money {
-
-	public Dollar(int amount, String currency) {
-		super(amount, currency);
-	}
-
-}
--- TDDMoneyExample/src/net/christiansons/mike/Franc.java
+++
@@ -1,9 +0,0 @@
-package net.christiansons.mike;
-
-public class Franc extends Money {
-
-	public Franc(int amount, String currency) {
-		super(amount, currency);
-	}
-	
-}
